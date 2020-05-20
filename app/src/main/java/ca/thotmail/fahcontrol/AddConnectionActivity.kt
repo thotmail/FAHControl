311368f3debc8b1e794828ded2541b627df6d3e3
Parva Thaker <parva@thotmail.ca>
2020-05-20 16:19:48 -0600

    Stage 0
    Added more validation for IP
    made it even more pretty?

--- app/src/main/java/ca/thotmail/fahcontrol/AddConnectionActivity.kt
+++ app/src/main/java/ca/thotmail/fahcontrol/AddConnectionActivity.kt
@@ -1,5 +1,7 @@
 package ca.thotmail.fahcontrol
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -7,17 +9,21 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_add_connection.*
+import java.lang.Integer.parseInt
 import java.util.regex.Pattern
 
 class AddConnectionActivity: AppCompatActivity() {
 
-    val partialIpAddressPattern: Pattern = Pattern.compile("^((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])\\.){0,3}"+
+    val partialIPAddressPattern: Pattern = Pattern.compile("^((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])\\.){0,3}"+
             "((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])){0,1}$")
+    val partOfIP = "([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])"
+    val IPAddressPattern: Pattern = Pattern.compile("$partOfIP\\.$partOfIP\\.$partOfIP\\.$partOfIP")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_connection)
         var passack = false
+        val oriColor = NicknamePrompt.currentTextColor
 
         IPAddrInput.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -30,17 +36,29 @@ class AddConnectionActivity: AppCompatActivity() {
 
             var prev = ""
             override fun afterTextChanged(s: Editable?) {
-                if(partialIpAddressPattern.matcher(s).matches()){
+                if(partialIPAddressPattern.matcher(s).matches()){
                     prev = s.toString()
                 }
                 else{
                     s?.replace(0, s.length, prev)
                 }
+                if(IPAddressPattern.matcher(s).matches()){
+                    IPAddrWarn.visibility = View.GONE
+                }
             }
         })
 
+
         AddConnectionSubmit.setOnClickListener {
             var submit = true
+            PasswordWarn.visibility = View.GONE
+            NicknamePrompt.setTextColor(oriColor)
+            NicknameInput.background = null
+
+            IPAddrPrompt.setTextColor(oriColor)
+            IPAddrWarn.visibility = View.GONE
+            IPAddrInput.background = null
+
             if (!passack && PasswordInput.text.toString().isNotEmpty()){
                 PasswordWarn.visibility = View.VISIBLE
                 passack = true
@@ -56,6 +74,13 @@ class AddConnectionActivity: AppCompatActivity() {
                 IPAddrInput.background = resources.getDrawable(R.drawable.border_error, null)
                 submit = false
             }
+            else if(!IPAddressPattern.matcher(IPAddrInput.text.toString()).matches()){
+                IPAddrWarn.visibility = View.VISIBLE
+                IPAddrPrompt.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorError))
+                IPAddrInput.background = resources.getDrawable(R.drawable.border_error, null)
+                submit = false
+            }
+
 
 
         }
